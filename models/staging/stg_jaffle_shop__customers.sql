3c0abd39e5469d45879e7194559f3e2705fbd25a
Jerin Jose <jerinjose1906@gmail.com>
2026-04-27 21:49:20 +0000

    added 2 tml files for jaffleshop and stripe

--- models/staging/stg_jaffle_shop__customers.sql
+++ models/staging/stg_jaffle_shop__customers.sql
@@ -1,6 +1,6 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+select id as customer_id, first_name, last_name
 
-from raw.jaffle_shop.customers
+from
+    {{ source("jaffle_shop", "customers") }}
+    -- replaced with the source macro above -- raw.jaffle_shop.customers
+    
